c532fa930bfece8c528875d0a841b90a7137fc13
David Eynon <davideynon@davids-macbook-pro.home>
2023-03-21 22:06:22 +0100

    95

--- clothing/src/routes/navigation/navigation.component.jsx
+++ clothing/src/routes/navigation/navigation.component.jsx
@@ -12,7 +12,7 @@ const Navigation = () => {
             <Link className='logo-container' to='/'>
                 <CrwnLogo className='logo' />
             </Link>
-            <div class='nav-links-container'>
+            <div className='nav-links-container'>
                 <Link className='nav-link' to='/shop'>
                     SHOP
                 </Link>
--- clothing/src/utils/firebase/firebase.utils.js
+++ clothing/src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import {
   GoogleAuthProvider 
 } from 'firebase/auth';
 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore'
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCwC2rQFgQWDVuM1T_7l6MpYzHppULqLZA",
@@ -27,3 +34,14 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+  export const db = getFirestore();
+  
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+  }
