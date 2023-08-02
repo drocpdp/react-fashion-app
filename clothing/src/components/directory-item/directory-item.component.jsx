5aabdf27bb125af9ad0c9982ec3420dd7b6bece3
drocpdp <davidreynon@gmail.com>
2023-08-02 18:01:38 +0900

    140, 141

--- clothing/src/components/directory-item/directory-item.component.jsx
+++ clothing/src/components/directory-item/directory-item.component.jsx
@@ -1,18 +1,21 @@
-import './directory-item.styles.scss'
+import { 
+  BackgroundImage, 
+  Body, 
+  DirectoryItemContainer,
+} from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
-    const { imageUrl, title} = category;
+    const { imageUrl, title } = category;
+    const imageurl = imageUrl;
     return (
-        <div className='directory-item-container'>
-          <div className='background-image' style={{
-            backgroundImage: `url(${imageUrl})`
-          }}/>
-          <div className='body'>
+        <DirectoryItemContainer>
+          <BackgroundImage imageurl={imageurl} />
+          <Body>
             <h2>{title}</h2>
             <p>Shop Now</p>
-          </div>
-        </div>   
-    )
-}
+          </Body>
+        </DirectoryItemContainer>
+    );
+};
 
-export default DirectoryItem
+export default DirectoryItem;
